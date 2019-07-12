0ae543cb3066d10ecd7c7ba628017b825e2b5db6
caozhen <cz19971224@126.com>
2019-07-12 23:31:44 +0800

    Owhat3

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ export default new VueRouter({
     routes:[
         {
             path:"/",
-            redirect:"/login"
+            redirect:"/home"
         },
         {
             path:"/home",
